3abeae2a983dfd644261a026446ecb27db347a6d
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-05-08 20:43:02 -0300

    feat: Configurando temas

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -16,9 +16,15 @@ const ButtonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
 
-  ${props => {
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white}
+
+  /* ${props => {
     return css `background-color: ${ButtonVariants[props.variant]}`
-  }}
+  }} */
 `
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  white: 'white',
+  primary: '#8257E6',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
