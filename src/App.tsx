d1c022c0d5f6f607383c94015844e9c88b3e6048
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-05-08 20:30:18 -0300

    feat: Adição do styled components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button/Index";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  )
+}
