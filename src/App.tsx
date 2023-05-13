914ef4661a762fb019701be58aa8324550d5d3c6
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-05-13 20:34:44 -0300

    feat: Adição do React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button/Index'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
