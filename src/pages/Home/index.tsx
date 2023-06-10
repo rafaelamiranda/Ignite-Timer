c1edff9c5377b6f88ad2db4b92ed4db8fbb57dcb
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-06-10 10:10:30 -0300

    feat: Criação da Home e do History

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,58 @@
+import { Play } from '@phosphor-icons/react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Front-end" />
+            <option value="Back-end" />
+            <option value="Mobile" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
