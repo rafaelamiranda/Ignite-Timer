75f15311c5644f98f68cf52c5768cf0017f17de4
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-06-18 18:53:34 -0300

    feat: Adição de status color

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components'
+import styled from 'styled-components'
 
 export const HistoryContainer = styled.main`
   flex: 1;
@@ -58,5 +58,29 @@ export const HistoryList = styled.div`
         padding-right: 1.5rem;
       }
     }
+  }
+`
+
+const STATUS_COLORS = {
+    yellow: 'yellow-500',
+    green: 'green-500',
+    red: 'red-500',
+} as const
+
+interface StatusProps {
+    statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]}
   }
 `
