6155dc1573d23e2809a80c25ee2a5af1b4792202
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-06-19 10:59:41 -0300

    fix: Adição de status color

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../../components/Header/index'
+import { Header } from '../../components/Header/index.tsx'
 import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -62,13 +62,13 @@ export const HistoryList = styled.div`
 `
 
 const STATUS_COLORS = {
-    yellow: 'yellow-500',
-    green: 'green-500',
-    red: 'red-500',
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
 } as const
 
 interface StatusProps {
-    statusColor: keyof typeof STATUS_COLORS
+  statusColor: keyof typeof STATUS_COLORS
 }
 
 export const Status = styled.span<StatusProps>`
@@ -81,6 +81,6 @@ export const Status = styled.span<StatusProps>`
     width: 0.5rem;
     height: 0.5rem;
     border-radius: 9999px;
-    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]}
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
